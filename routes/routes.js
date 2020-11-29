a9181720f0968d2eda0eb81387b7c3ea044bdec5
Kaylee Horner <kayleeahorner@gmail.com>
2020-11-28 23:12:07 -0600

    splitting routes into 2 files

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,17 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
 
-router.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-  
+
 router.get("/notes", (_req,res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-const notes = [];
+router.get("/", (req,res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-router.post("/notes", function (req, res) {
-  const newNote = req.body;
-  console.log(newNote);
-  notes.push(newNote);
-  res.json(newNote);
+router.get("*", (_req,res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
