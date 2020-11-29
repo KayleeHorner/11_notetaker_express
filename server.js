63eff002a62b75cec5ad375133c134680037d3fe
Kaylee Horner <kayleeahorner@gmail.com>
2020-11-28 23:12:40 -0600

    added 2nd route file to server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // Require/import the HTTP module
 const express = require("express");
 const app = express();
-const routes = require('./routes/routes.js')
+const routes = require("./routes/routes.js");
+const apiRoutes = require("./routes/apiroutes.js");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(routes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
